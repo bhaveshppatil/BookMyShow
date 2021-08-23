1e4a6ce031db3a19f55b86a7e5f1eb782adee688
MuraliKrishnaSundara <Murali.Sundara@ssipmt.com>
2021-08-23 20:49:47 +0530

    Splash Screen Done

--- app/src/main/java/com/example/bookmyshow/MainActivity.java
+++ app/src/main/java/com/example/bookmyshow/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.bookmyshow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(splashIntent);
+                finish();
+            }
+        }, 1000);
     }
 }
