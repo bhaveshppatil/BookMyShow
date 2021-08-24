ffc6b71041e80882dcc81cc0edb3871f915b3813
Reeta Thakur <thakurreeta@gmail.com>
2021-08-24 13:37:23 +0530

    add slider

--- app/src/main/java/com/example/bookmyshow/MainActivity.java
+++ app/src/main/java/com/example/bookmyshow/MainActivity.java
@@ -4,11 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    SliderView sliderView;
+    private ArrayList<Integer> list=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sliderView=findViewById(R.id.image_slider);
+        list.add(R.drawable.image1);
+        list.add(R.drawable.image2);
+        list.add(R.drawable.image3);
+        setImges();
     }
+
+    private void setImges() {
+        SliderAdapter adapter=new SliderAdapter(list);
+        sliderView.setSliderAdapter(adapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
+    }
+
+
 }
