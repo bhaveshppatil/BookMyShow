2b1ff3825cc318a9f5b1c8ea48332d8bba50da3a
MuraliKrishnaSundara <Murali.Sundara@ssipmt.com>
2021-08-24 15:49:55 +0530

    Update from Murali

--- app/src/main/java/com/example/bookmyshow/HomeFragment.java
+++ app/src/main/java/com/example/bookmyshow/HomeFragment.java
@@ -1,7 +1,9 @@
 package com.example.bookmyshow;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,23 +19,30 @@ import java.util.ArrayList;
 public class HomeFragment extends Fragment {
     SliderView sliderView;
     private ArrayList<Integer> list = new ArrayList<>();
-/*
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        sliderView = view.findViewById(R.id.image_slider);
-        list.add(R.drawable.image1);
-        list.add(R.drawable.image2);
-        list.add(R.drawable.image3);
-        setImges();
-    }
 
-    private void setImges() {
-        SliderAdapter adapter = new SliderAdapter(list);
-        sliderView.setSliderAdapter(adapter);
-        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
-        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
-        sliderView.startAutoCycle();
+    /*
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            sliderView = view.findViewById(R.id.image_slider);
+            list.add(R.drawable.image1);
+            list.add(R.drawable.image2);
+            list.add(R.drawable.image3);
+            setImges();
+        }
+
+        private void setImges() {
+            SliderAdapter adapter = new SliderAdapter(list);
+            sliderView.setSliderAdapter(adapter);
+            sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+            sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+            sliderView.startAutoCycle();
+        }
+    */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
-*/
 }
