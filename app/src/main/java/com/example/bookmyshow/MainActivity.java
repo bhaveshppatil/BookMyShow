608c5f254eaa371caa5772227e3724f8ac5596a3
MuraliKrishnaSundara <Murali.Sundara@ssipmt.com>
2021-08-24 17:01:27 +0530

    Update from Murali

--- app/src/main/java/com/example/bookmyshow/MainActivity.java
+++ app/src/main/java/com/example/bookmyshow/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.example.bookmyshow.Home.HomeActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
