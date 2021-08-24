5273d0bcb906c50cb37e400381625468af0d307f
Reeta Thakur <thakurreeta@gmail.com>
2021-08-24 18:07:19 +0530

    add second slider

---
+++ app/src/main/java/com/example/bookmyshow/imageSliderItem.java
@@ -0,0 +1,37 @@
+package com.example.bookmyshow;
+
+public class imageSliderItem {
+    private int images;
+    private String movieName;
+    private String movieDuration;
+    private String movieLanguage;
+    private String movieDescription;
+
+    public imageSliderItem(int images, String movieName, String movieDuration, String movieLanguage, String movieDescription) {
+        this.images = images;
+        this.movieName = movieName;
+        this.movieDuration = movieDuration;
+        this.movieLanguage = movieLanguage;
+        this.movieDescription = movieDescription;
+    }
+
+    public int getImages() {
+        return images;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public String getMovieDuration() {
+        return movieDuration;
+    }
+
+    public String getMovieLanguage() {
+        return movieLanguage;
+    }
+
+    public String getMovieDescription() {
+        return movieDescription;
+    }
+}
