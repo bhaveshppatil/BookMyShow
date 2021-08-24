37280fd55be290d556bd1cfca67f7cc4de12bb2c
MuraliKrishnaSundara <Murali.Sundara@ssipmt.com>
2021-08-24 16:28:39 +0530

    Update from Murali

--- app/src/main/java/com/example/bookmyshow/HomeFragment.java
+++ app/src/main/java/com/example/bookmyshow/HomeFragment.java
@@ -17,44 +17,6 @@ import com.smarteist.autoimageslider.SliderViewAdapter;
 import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
-    SliderView sliderView, sliderView1;
-    private ArrayList<Integer> list = new ArrayList<>();
-    private ArrayList<imageSliderItem> items = new ArrayList<>();
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        setContentView(R.layout.fragment_home);
-        sliderView = findViewById(R.id.image_slider);
-        sliderView1 = findViewById(R.id.image_slider_two);
-        list.add(R.drawable.image1);
-        list.add(R.drawable.image2);
-        list.add(R.drawable.image3);
-        setImges();
-        setDataForSecondSlider();
-        setSliderForSecond();
-    }
-
-    private void setSliderForSecond() {
-        ImageSliderAdapter sliderAdapter = new ImageSliderAdapter(items);
-        sliderView1.setSliderAdapter(sliderAdapter);
-        sliderView1.setIndicatorAnimation(IndicatorAnimationType.WORM);
-        sliderView1.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
-        sliderView1.startAutoCycle();
-    }
-
-    private void setDataForSecondSlider() {
-        items.add(new imageSliderItem(R.drawable.magialnight, "On Magical Night", "1h 50m, Comedy,18+", "English, Hindi, French", "this is a greate moive of making"));
-        items.add(new imageSliderItem(R.drawable.beckyposter, "On Magical Night", "1h 50m, Comedy,18+", "English, Hindi, French", "this is a greate moive of making"));
-    }
-
-    private void setImges() {
-        SliderAdapter adapter = new SliderAdapter(list);
-        sliderView.setSliderAdapter(adapter);
-        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
-        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
-        sliderView.startAutoCycle();
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
