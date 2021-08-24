40a59a77bf776334a01926295c200fa688827ac7
MuraliKrishnaSundara <Murali.Sundara@ssipmt.com>
2021-08-24 20:45:30 +0530

    Update from Murali _ Buzz Recycler View Done

--- app/src/main/java/com/example/bookmyshow/MainActivity.java
+++ app/src/main/java/com/example/bookmyshow/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 import com.example.bookmyshow.Home.HomeActivity;
+
 import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
