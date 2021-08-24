4b8dd9645141d8a35912d6f923c0982e9529e10b
MuraliKrishnaSundara <Murali.Sundara@ssipmt.com>
2021-08-24 10:30:53 +0530

    Home Screen UI Done

--- app/src/main/java/com/example/bookmyshow/HomeActivity.java
+++ app/src/main/java/com/example/bookmyshow/HomeActivity.java
@@ -1,14 +1,45 @@
 package com.example.bookmyshow;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initViews();
+        setAdapter();
+    }
+
+    private void setAdapter() {
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), getLifecycle());
+        viewPager2.setAdapter(pagerAdapter);
+        new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                if (position == 0)
+                    tab.setText("Names");
+                else if (position == 1)
+                    tab.setText("Places");
+                else if (position == 2)
+                    tab.setText("Tourism");
+            }
+        }).attach();
+    }
+
+    private void initViews() {
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager2 = findViewById(R.id.viewPager);
     }
 }
---
+++ app/src/main/java/com/example/bookmyshow/PagerAdapter.java
@@ -0,0 +1,33 @@
+package com.example.bookmyshow;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class PagerAdapter extends FragmentStateAdapter {
+
+    public PagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return new BuzzFragment();
+            case 2:
+                return new ProfileFragment();
+        }
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
