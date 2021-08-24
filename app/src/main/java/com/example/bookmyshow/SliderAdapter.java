9da0eb9b3c8e1c5cf36407d1fbfb0db029e2eb67
Reeta Thakur <thakurreeta@gmail.com>
2021-08-24 18:07:19 +0530

    add second slider

---
+++ app/src/main/java/com/example/bookmyshow/SliderAdapter.java
@@ -0,0 +1,42 @@
+package com.example.bookmyshow;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.smarteist.autoimageslider.SliderViewAdapter;
+
+import java.util.ArrayList;
+
+public class SliderAdapter extends SliderViewAdapter<SliderAdapter.Holder> {
+    private ArrayList<Integer> imageList;
+    public SliderAdapter(ArrayList<Integer> imageList){
+        this.imageList=imageList;
+    }
+
+    @Override
+    public Holder onCreateViewHolder(ViewGroup parent) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.imageslider_first_layout,parent,false);
+        return new Holder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(Holder viewHolder, int position) {
+        viewHolder.imageView.setImageResource(imageList.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return imageList.size();
+    }
+
+    public class Holder extends SliderViewAdapter.ViewHolder{
+        ImageView imageView;
+
+        public Holder(View itemView) {
+            super(itemView);
+            imageView=itemView.findViewById(R.id.image_view);
+        }
+    }
+}
