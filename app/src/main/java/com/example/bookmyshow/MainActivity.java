e2c67323d1fa29e7f7d55fe13ce10f523ef3fcee
Reeta Thakur <thakurreeta@gmail.com>
2021-08-24 16:04:22 +0530

    add second slider

--- app/src/main/java/com/example/bookmyshow/MainActivity.java
+++ app/src/main/java/com/example/bookmyshow/MainActivity.java
@@ -11,18 +11,35 @@ import com.smarteist.autoimageslider.SliderView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    SliderView sliderView;
+    SliderView sliderView,sliderView1;
     private ArrayList<Integer> list=new ArrayList<>();
+    private ArrayList<imageSliderItem> items=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sliderView=findViewById(R.id.image_slider);
+        sliderView1=findViewById(R.id.image_slider_two);
         list.add(R.drawable.image1);
         list.add(R.drawable.image2);
         list.add(R.drawable.image3);
         setImges();
+        setDataForSecondSlider();
+        setSliderForSecond();
+    }
+
+    private void setSliderForSecond() {
+        ImageSliderAdapter sliderAdapter=new ImageSliderAdapter(items);
+        sliderView1.setSliderAdapter(sliderAdapter);
+        sliderView1.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView1.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView1.startAutoCycle();
+    }
+
+    private void setDataForSecondSlider() {
+        items.add(new imageSliderItem(R.drawable.magialnight,"On Magical Night","1h 50m, Comedy,18+","English, Hindi, French","this is a greate moive of making"));
+        items.add(new imageSliderItem(R.drawable.beckyposter,"On Magical Night","1h 50m, Comedy,18+","English, Hindi, French","this is a greate moive of making"));
     }
 
     private void setImges() {
